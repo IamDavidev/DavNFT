306880a3bc67c9b862a164a7e4fad6eb2b4cd90d
itsDavidev <davidlez2mgk@gmail.com>
2022-05-18 21:16:54 -0500

    update davNFt

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 function App() {
-	return <div className='bg-black min-h-screen w-ful text-white'></div>;
+	return (
+		<div className='bg-black p-4  min-h-screen w-ful text-white'>
+			welcome to <span className='text-purple'>Dav NFT Marketplace</span>
+			<button className='border-0 bg-purpleLite m-4 text-black py-4 px-8 rounded-3xl animate-bounce'>
+				hola{' '}
+			</button>
+			<button class='animate-spin border-o'>
+				other animatin with tailwind css
+			</button>
+		</div>
+	);
 }
 
 export default App;
